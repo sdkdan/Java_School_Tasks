bd91a049691b9f556a46fe08487c7d6963f427de
lihovs37 <34690904+lihovs37@users.noreply.github.com>
2018-01-20 18:07:33 +0300

    Update Calculator.java

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,1 +1,10 @@
+package com.tsystems.javaschool.tasks;
 
+public class Calculator extends CalculatorImpl {
+    public static void main(String[] args) {
+        CalculatorImpl c = new CalculatorImpl();
+        System.out.println(c.evaluate("(1+38)*4-5")); // Result: 151
+        System.out.println(c.evaluate("7*6/2+8")); // Result: 29
+        System.out.println(c.evaluate("-12)1//(")); // Result: null
+    }
+}
