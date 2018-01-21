a111b19b255acd48e4c4314c30b80d159eee004f
lihovs37 <34690904+lihovs37@users.noreply.github.com>
2018-01-21 13:53:59 +0300

    Update Pyramid.java

--- Pyramid/Pyramid.java
+++ Pyramid/Pyramid.java
@@ -1,1 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
 
+public class Pyramid {
+    public static void main(String[] args) {
+        PyramidBuilder p = new PyramidBuilder();
+        List<Integer> inputNumbers = Arrays.asList(11, 1, 21, 12, 3, 16, 2, 13, 9, 4, 17, 5, 14, 10, 18, 8, 7, 19, 15, 6, 20);
+        p.buildPyramid(inputNumbers);
+        }
+    }
